a1c2c9ca6e1c8023147c30b66b66c3adc8ed2587
soko-no-otaku <soko.no.otaku@gmail.com>
2017-10-25 02:00:57 +0900

    Add files via upload

---
+++ compress_all.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+require 'shellwords'
+
+Dir::mkdir('output') unless Dir::exist?('output')
+
+Dir.glob("*.{MOV,m4v,mp4}").each do |unescaped_input_filename|
+  input_filename = Shellwords.escape(unescaped_input_filename)
+  output_filename = Shellwords.escape(File.basename(unescaped_input_filename, '.*') + '.mp4')
+
+  system "ffmpeg -i #{input_filename} -vf scale=iw/2:ih/2 ./output/#{output_filename}"
+end
